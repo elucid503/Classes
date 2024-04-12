e4e71eff250ffb7fcf02dfde40e9ec28e02fefc8
elucid503 <paulujlaky@gmail.com>
2024-04-12 14:31:00 -0400

    Add functions lab from 4/12

---
+++ CPSC-1131/C/Labs/lab25.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+// Protos
+
+void HelloFun(void);
+
+void HelloGreetingFun(int NumberChosen);
+
+int DoArithmetic(int Numbers[]);
+
+// Main
+
+int main() {
+
+    HelloFun();
+
+    printf("Enter a number: ");
+
+    int NumberChosen;
+
+    scanf("%d", &NumberChosen);
+
+    HelloGreetingFun(NumberChosen); // Any other param type would require a pointer
+
+    int Numbers[] = {1, 2, 3};
+
+    printf("The result is %d\n", DoArithmetic(Numbers));
+
+    return 0;
+
+}
+
+// Declarations 
+
+void HelloFun(void) {
+
+    printf("Hello, C!\n");
+
+}
+
+void HelloGreetingFun(int NumberChosen) {
+
+    printf("Your number is %d\n", NumberChosen);
+    
+}
+
+int DoArithmetic(int Numbers[]) {
+
+    int SumOfFirstAndThird = Numbers[0] + Numbers[2];
+
+    int Subtracted = SumOfFirstAndThird - Numbers[1];
+
+    return Subtracted; // Should be 2 if given { 1, 2, 3 }
+}
