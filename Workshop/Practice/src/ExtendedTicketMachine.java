ab26174ac8bfe865312bc561f2a9c50b72e947a5
elucid503 <elucid@duck.com>
2024-09-11 09:08:08 -0400

    All kinds of things

---
+++ Workshop/Practice/src/ExtendedTicketMachine.java
@@ -0,0 +1,117 @@
+public class ExtendedTicketMachine {
+
+    // The price of a ticket
+
+    private final int priceOfTicket;
+
+    // The amount of money entered
+
+    private int balance;
+
+    // Constructor
+
+    /**
+     * Creates a 'ticket machine' with a given price for each ticket
+     * Note that the price must be greater than zero
+     */
+    public ExtendedTicketMachine(int cost) {
+
+        if (cost <= 0) {
+
+            throw new IllegalArgumentException("Cost must be greater than zero.");
+
+        }
+
+        priceOfTicket = cost;
+        balance = 0;
+
+    }
+
+    // Getters
+
+    /**
+     * Return the price of a ticket
+     */
+    public int getPriceOfTicket() {
+
+        return priceOfTicket;
+
+    }
+
+    /**
+     * Return the balance
+     */
+    public int getBalance() {
+
+        return balance;
+
+    }
+
+    // Methods
+
+    /**
+     * Add money to the balance
+     */
+    public void addMoney(int amount) {
+
+        balance += amount;
+
+    }
+
+    /**
+     * Remove money from the balance
+     */
+
+    public void removeMoney(int amount) {
+
+        balance -= amount;
+
+    }
+
+    /**
+     * Buy and print a ticket
+     * Returns an ambiguous error if balance is not high enough.
+     * */
+    public void buyAndPrintTicket()  {
+
+        boolean wasSuccessful = buyTicket();
+
+        if (!wasSuccessful) {
+
+            System.out.println("Balance is too low. You must insert at least: " + (priceOfTicket - balance) + " more dollars.");
+            return;
+
+        }
+
+        // 'Print' a ticket
+
+        System.out.println("##################");
+        System.out.println("# The BlueJ Line");
+        System.out.println("# Ticket");
+        System.out.println("# " + priceOfTicket + " dollars.");
+        System.out.println("##################");
+
+    }
+
+    // Private methods
+
+    /**
+     * Try to 'buy' a ticket with the current balance
+     * Returns true if successful, false otherwise
+     */
+    private boolean buyTicket() {
+
+        if (balance < priceOfTicket) {
+
+            return false;
+
+        } else {
+
+            balance -= priceOfTicket;
+            return true;
+
+        }
+
+    }
+
+}
