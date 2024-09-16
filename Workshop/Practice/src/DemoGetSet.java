361a4ccb9e537254aa383ee1bb5d5adfad07037b
elucid503 <elucid@duck.com>
2024-09-16 13:07:58 -0400

    Demo getters and setters

---
+++ Workshop/Practice/src/DemoGetSet.java
@@ -0,0 +1,13 @@
+public class DemoGetSet {
+
+    public static void main(String[] args) {
+
+        GetSet getSet = new GetSet();
+
+        getSet.setNumber(10);
+
+        System.out.println(getSet.getNumber());
+
+    }
+
+}
---
+++ Workshop/Practice/src/GetSet.java
@@ -0,0 +1,17 @@
+public class GetSet {
+
+    private int Number = 0;
+
+    public void setNumber(int Number) {
+
+        this.Number = Number;
+
+    }
+
+    public int getNumber() {
+
+        return Number;
+
+    }
+
+}
