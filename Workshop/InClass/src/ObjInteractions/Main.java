3d71d44da3a2e11a2353f3bc24da73931e131b2b
elucid503 <elucid@duck.com>
2024-09-23 13:02:43 -0400

    Fix stupidity

--- Workshop/InClass/src/ObjInteractions/Main.java
+++ Workshop/InClass/src/ObjInteractions/Main.java
@@ -2,16 +2,14 @@ package ObjInteractions;
 
 public class Main {
 
-    Message message = new Message();
-    Person person = new Person();
+    static Message message = new Message();
+    static Person person = new Person();
 
     public static void main(String[] args) {
 
-        Main main = new Main(); // need to make this class since this function is static
-
-        main.message.createMessage("Hello, World!");
-        main.person.setReceivedMessage(main.message.message);
-        main.person.showMessage();
+        message.createMessage("Hello, World!");
+        person.setReceivedMessage(message.message);
+        person.showMessage();
 
     }
 
