6119fa1a47f2b45b0671b3c5061731c078c08b18
elucid503 <elucid@duck.com>
2024-09-26 13:45:20 -0400

    Files from 9/26

--- Workshop/InClass/src/ObjInteractions/Main.java
+++ Workshop/InClass/src/ObjInteractions/Main.java
@@ -3,13 +3,17 @@ package ObjInteractions;
 public class Main {
 
     static Message message = new Message();
-    static Person person = new Person();
+
+    static Person person1 = new Person("Person 1");
+    static Person person2 = new Person("Person 2");
 
     public static void main(String[] args) {
 
         message.createMessage("Hello, World!");
-        person.setReceivedMessage(message.message);
-        person.showMessage();
+        person1.setMessage(message);
+        person1.showMessage();
+        person1.sendMessage(person1.receivedMessage, person2);
+        person2.showMessage();
 
     }
     
