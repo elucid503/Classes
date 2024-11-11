1190fd9af7bb971ab4343cb7ccf5467692fc0690
elucid503 <elucid@duck.com>
2024-11-11 13:40:35 -0500

    Random files from 11/11 class...

---
+++ Workshop/Classwork/src/Class19Practice/Main.java
@@ -0,0 +1,13 @@
+package Class19Practice;
+
+public class Main {
+
+    public static void main(String[] argv) {
+
+        SupportSystem techSupport = new SupportSystem();
+
+        techSupport.start();
+
+    }
+
+}
