d1fdefcb48b6bb35c5118ce127c4746044017549
elucid503 <elucid@duck.com>
2024-11-18 12:48:20 -0500

    New Files

--- Workshop/Classwork/src/Class20Practice/AnimalBehavior.java
+++ Workshop/Classwork/src/Class20Practice/AnimalBehavior.java
@@ -1,4 +1,14 @@
 package Class20Practice;
 
 public interface AnimalBehavior {
+
+    public static final String name = "Animal";
+
+    public abstract void eat();
+    public abstract void sleep();
+
+    public default void run() {
+        System.out.println("Animal is running");
+    }
+
 }
--- Workshop/Classwork/src/Class20Practice/Cat.java
+++ Workshop/Classwork/src/Class20Practice/Cat.java
@@ -1,4 +1,39 @@
 package Class20Practice;
 
-public class Cat {
+public class Cat extends Animal {
+
+    static int numberOfCatsCreated = 0;
+
+    public Cat(String name) {
+
+        super(name);
+        numberOfCatsCreated++;
+
+        this.type = "cat";
+
+    }
+
+    @Override
+    public void run() {
+
+        System.out.println("The cat is running!");
+
+    }
+
+    @Override
+    public void eat() {
+
+        // Demo of calling super
+
+        System.out.println("Feeding the cat");
+        super.eat();
+
+    }
+
+    public String getType() {
+
+        return this.type;
+
+    }
+
 }
