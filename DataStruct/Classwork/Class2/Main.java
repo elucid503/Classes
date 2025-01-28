363a15b52c9848ff92a5621b7d3fc58431260bb8
elucid503 <elucid@duck.com>
2025-01-28 09:27:28 -0500

    Add new stuff

---
+++ DataStruct/Classwork/Class2/Main.java
@@ -0,0 +1,17 @@
+package Classwork.Class2;
+
+import java.util.logging.*;
+
+public class Main {
+
+    static Logger logger = Logger.getLogger("Main");
+
+    public static void main(String[] argv) {
+
+        logger.setLevel(Level.ALL);
+
+        logger.info("Hello World!");
+        
+    }
+    
+}
---
+++ DataStruct/Examples/Ex2/GameEntry.java
@@ -0,0 +1,33 @@
+
+public class GameEntry {
+
+	private String name;
+	private int score;
+	
+	public GameEntry(String n, int s) {
+		name = n;
+		score = s;
+		
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getScore() {
+		return score;
+	}
+
+	public void setScore(int score) {
+		this.score = score;
+	}
+	
+	public String toString() {
+		return name + " " + score;
+	}
+	
+}
