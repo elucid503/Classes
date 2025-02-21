4cbee7dce38f99f95ffc3f23065bb6de2832b781
elucid503 <elucid@duck.com>
2025-02-21 10:32:53 -0500

    Project work

--- DataStruct/Project/Util/Operators.java
+++ DataStruct/Project/Util/Operators.java
@@ -17,6 +17,22 @@ public enum Operators {
 
         this.operator = operator;
 
-    }                    
+    }
+    
+    public static Operators getOperator(String s) {
+        
+        for (Operators entry : Operators.values()) {
+            
+            if (entry.operator.equals(s)) {
+                
+                return entry;
+                
+            }
+            
+        }
+        
+        return null;
+        
+    }
     
 }
