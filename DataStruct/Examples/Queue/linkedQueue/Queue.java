08c5817d836a8fb8bbf17ffec6fc1c6df7d56b26
elucid503 <elucid@duck.com>
2025-02-25 09:31:58 -0500

    Remove problematic example dirs

--- DataStruct/Examples/Queue/linkedQueue/Queue.java
+++
@@ -1,9 +0,0 @@
-package linkedQueue;
-public interface Queue <T>
-{
-	public boolean empty();
-	public boolean full();
-	public boolean enqueue(T data);
-	public T dequeue();	
-	public void display();
-}
--- DataStruct/Examples/Stack/ArrayStack/Stack.java
+++
@@ -1,9 +0,0 @@
-package ArrayStack;
-
-public interface Stack<E> {
-	int size();
-	boolean isEmpty();
-	E top();
-	void push(E element);
-	E pop();
-}
